1d7ec532f2fce7d55026fe5972efe4e75e977e52
salmi2 <amine.salmi.solicode@gmail.com>
2022-10-19 16:57:34 +0100

    .

--- app/Http/Controllers/ApprenantsController.php
+++ app/Http/Controllers/ApprenantsController.php
@@ -7,14 +7,16 @@ use App\Models\ListApprenant;
 
 class ApprenantsController extends Controller
 {
-    public function show(){
-        return view('index');
-    }
+    // public function show(){
+    //     return view('index');
+    // }
 
     public function select(){
         $data = ListApprenant::all();
         return view('index', compact('data'));
     }
+
+
     public function insertData(Request $request){
         $obj_models = new ListApprenant();
         $obj_models->name = $request->input;
